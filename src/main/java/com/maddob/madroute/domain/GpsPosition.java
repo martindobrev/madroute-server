bc6e8b34da66fa6f3d44c77a59428de9e1d18d8e
martindobrev <martin@dobrev.eu.com>
2017-12-12 00:02:23 +0200

    Adding the parser from some guy's github repo - to be refactored!

---
+++ src/main/java/com/maddob/madroute/domain/GpsPosition.java
@@ -0,0 +1,11 @@
+package com.maddob.madroute.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+public class GpsPosition {
+    // TODO: Define GpsPosition and continue with the parser
+}
