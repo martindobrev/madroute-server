445b21a0aed1b9eafde70b6ce8844357131b19a2
martindobrev <martin@dobrev.eu.com>
2017-12-11 23:13:41 +0200

    Initial commit

---
+++ src/main/java/com/maddob/madroute/MadrouteApplication.java
@@ -0,0 +1,12 @@
+package com.maddob.madroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrouteApplication.class, args);
+	}
+}
