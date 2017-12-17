b1cd24cd44c495958404ebe1122bc6d4db549bcc
martindobrev <martin@dobrev.eu.com>
2017-12-17 23:50:37 +0200

    another bootstrap video added

--- src/main/java/com/maddob/madroute/bootstrap/BootstrapSomeData.java
+++ src/main/java/com/maddob/madroute/bootstrap/BootstrapSomeData.java
@@ -18,6 +18,9 @@ public class BootstrapSomeData implements ApplicationListener<ContextRefreshedEv
     @Value(value = "classpath:bootstrap/17110502.LOG")
     private Resource gpsDataTestRide;
 
+    @Value(value = "classpath:bootstrap/17111003.LOG")
+    private Resource gpsDataRide2Work;
+
     private final MadRouteRepository madRouteRepository;
 
     public BootstrapSomeData(MadRouteRepository madRouteRepository) {
@@ -33,21 +36,34 @@ public class BootstrapSomeData implements ApplicationListener<ContextRefreshedEv
         sofiaRide.setName("Up to Vitosha Mountain");
         sofiaRide.setVideoId("1savMPQRWvg");
 
+        sofiaRide.setGpsData(getResourceBytes(gpsDataTestRide));
+        madRouteRepository.save(sofiaRide);
+
+        final MadRoute ride2work = new MadRoute();
+        ride2work.setDescription("Just a rest ride to work on my first fixie");
+        ride2work.setLocation("Sofia, Bulgaria");
+        ride2work.setName("Ride to work");
+        ride2work.setVideoId("iAqC3FNJboo");
+        ride2work.setGpsData(getResourceBytes(gpsDataRide2Work));
+        madRouteRepository.save(ride2work);
+
+    }
+
+    private Byte[] getResourceBytes(final Resource resource) {
         try {
-            final int length = (int) gpsDataTestRide.contentLength();
-            final Byte[] bytes = new Byte[(int) gpsDataTestRide.contentLength()];
-            final InputStream inputStream = gpsDataTestRide.getInputStream();
+            final int length = (int) resource.contentLength();
+            final Byte[] bytes = new Byte[(int) resource.contentLength()];
+            final InputStream inputStream = resource.getInputStream();
 
             int byteIndex = 0;
             int byteRead;
             while ((byteRead = inputStream.read()) != -1) {
                 bytes[byteIndex++] = (byte) byteRead;
             }
-            sofiaRide.setGpsData(bytes);
-
+            return bytes;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        madRouteRepository.save(sofiaRide);
     }
 }
